28ad8c2606309ea99bb6f6010fccf0d64be99967
AngelSantiag085 <angelraulsantiagohdez@gmail.com>
2026-07-01 01:05:49 -0600

    Add files via upload
    primer web oficial

---
+++ script.js
@@ -0,0 +1,32 @@
+new Swiper(".letrasSwiper", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: ".letrasSwiper .swiper-pagination",
+        clickable: true,
+    },
+});
+
+new Swiper(".numerosSwiper", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: ".numerosSwiper .swiper-pagination",
+        clickable: true,
+    },
+});
+
+new Swiper(".mamparasSwiper", {
+    loop:true,
+    autoplay: {
+        delay:3000,
+    },
+    pagination: {
+        el: ".mamparasSwiper .swiper-pagination",
+        clickable:true,
+    },
+});

b92f9a80738a6ae6f80e6d4bb5beefb063b88855
AngelSantiag085 <angelraulsantiagohdez@gmail.com>
2026-07-01 23:21:34 -0600

    Add files via upload
    cambios de clases y arreglo del carrusel en las fotos

--- script.js
+++ script.js
@@ -16,32 +16,47 @@ new Swiper(".letrasSwiper", {
 
 new Swiper(".numerosSwiper", {
     loop: true,
+    speed:800,
     autoplay: {
-        delay: 3000,
+        delay: 4000,
+        disableOnInteraction:false,
+        pauseOnMouseEnter:true,
     },
     pagination: {
         el: ".numerosSwiper .swiper-pagination",
         clickable: true,
     },
+    grabCursor:true,
+    touchEventsTarget:"container",
 });
 
 new Swiper(".mamparasSwiper", {
     loop:true,
+    speed:800,
     autoplay: {
-        delay:3000,
+        delay:4000,
+        disableOnInteraction:false,
+        pauseOnMouseEnter:true,
     },
     pagination: {
         el: ".mamparasSwiper .swiper-pagination",
         clickable:true,
     },
+    grabCursor:true,
+    touchEventsTarget:"container",
 });
 new Swiper(".barrasSwiper", {
     loop: true,
+    speed:800,
     autoplay: {
-        delay: 3000,
+        delay: 4000,
+        disableOnInteraction:false,
+        pauseOnMouseEnter:true,
     },
     pagination: {
         el: ".barrasSwiper .swiper-pagination",
         clickable: true,
     },
+    grabCursor:true,
+    touchEventsTarget:"container",
 });
